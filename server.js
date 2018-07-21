8b1c2d784d92bbc4a5ca57b72a27d9e8bd7ac17c
Mehmet Ali <mehmetalie94@unet.univie.ac.at>
2018-07-21 21:02:05 +0200

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-polished-71854',
     user : 'postgres',
     password : '2kafadar',
     database : 'smart-brain'
